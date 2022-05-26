a28fb6e6cfd2736791695639ce211662fa367852
Thakur Viraj Singh <75830024+singhhimanshu31@users.noreply.github.com>
2022-05-26 22:02:33 +0530

    navigation update

---
+++ src/component/Hotlinks.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hotlinks = () => {
+    return (
+        <div>
+            <nav className="nav nav-pills nav-fill bg-dark">
+                <a className="nav-link" aria-current="page" href="#">GT vs RR</a>
+                <a className="nav-link" href="#">LSG vs RCB</a>
+                <a className="nav-link" href="#">RR vs RCB</a>
+                <a className="nav-link">GT vs TBA</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Hotlinks
